ce3b4ecc76d498f2fb40540a4e993069be75c92f
Tekknobot <thrillatron@hotmail.com>
2025-06-09 20:52:50 -0400

    Push for break.

--- Scripts/Conclusion.gd
+++ Scripts/Conclusion.gd
@@ -26,7 +26,7 @@ func _ready():
 func set_result(result: String, stats: Dictionary, rewards: Dictionary) -> void:
 	# Update the result label without using a ternary.
 	if result == "win":
-		result_label.text = "Victory!"
+		result_label.text = "Upgrades!"
 		# Play the victory sound.
 		if victory_sound:
 			audio_player.stream = victory_sound
