517486c14919000efe9adad399c41cb0be3dfd3d
Tekknobot <thrillatron@hotmail.com>
2025-07-30 20:26:41 -0400

    Use demo video for now.

--- Scripts/TutorialMenu.gd
+++ Scripts/TutorialMenu.gd
@@ -1,33 +1,16 @@
 extends Control
 
-@onready var push_btn      		= $CenterContainer/VBoxContainer/PushBtn
+@onready var demo_btn      		= $CenterContainer/VBoxContainer/DemoBtn
 @onready var impact_btn		= $CenterContainer/VBoxContainer/ImpactBtn
 @onready var abilities_btn 		= $CenterContainer/VBoxContainer/AbilitiesBtn
 @onready var tips_btn      		= $CenterContainer/VBoxContainer/TipsBtn
 @onready var back_btn      		= $CenterContainer/VBoxContainer/BackBtn
-@onready var player          	= $PanelContainer/VideoPlayer
-@onready var panel_container 	= $PanelContainer
+@onready var player          	= $VideoPlayer
 
 # Define playlists for each module
 var playlists = {
-	"push": [
-		"res://Video/Tutorial/push_1.ogv",
-		"res://Video/Tutorial/push_2.ogv",
-		"res://Video/Tutorial/push_3.ogv"
-	],
-	"ranged": [
-		"res://Video/Tutorial/push_4.ogv",
-		"res://Video/Tutorial/push_5.ogv",
-		"res://Video/Tutorial/push_6.ogv"
-	],	
-	"abilities": [
-		"res://Video/Tutorial/dash.ogv",
-		"res://Video/Tutorial/double_jump.ogv",
-		"res://Video/Tutorial/grapple.ogv"
-	],
-	"tips": [
-		"res://Video/Tutorial/physics_tweaks.ogv",
-		"res://Video/Tutorial/collision_shapes.ogv"
+	"demo": [
+		"res://Video/Tutorial/demo_1.ogv"
 	]
 }
 
@@ -36,7 +19,7 @@ var current_index : int = 0
 
 func _ready() -> void:
 	# connect with bound arguments instead of lambdas
-	push_btn.pressed.connect(Callable(self, "_start_tutorial").bind("push"))
+	demo_btn.pressed.connect(Callable(self, "_start_tutorial").bind("demo"))
 	impact_btn.pressed.connect(Callable(self, "_start_tutorial").bind("ranged"))
 	abilities_btn.pressed.connect(Callable(self, "_start_tutorial").bind("abilities"))
 	tips_btn.pressed.connect(Callable(self, "_start_tutorial").bind("tips"))
@@ -59,7 +42,6 @@ func _start_tutorial(module_name: String) -> void:
 func _play_current_video() -> void:
 	player.stream = ResourceLoader.load(current_list[current_index])
 	player.visible = true
-	panel_container.visible = true
 	player.play()
 
 func _on_Video_finished() -> void:
@@ -69,7 +51,6 @@ func _on_Video_finished() -> void:
 	else:
 		# End of module: hide player, show menu again
 		player.visible = false
-		panel_container.visible = false
 
 func _on_Back_pressed() -> void:
 	# Return to main menu
